5b76875f39811cf3bb5c0c0adb62fcfea59b6c72
sreen98 <sreenath.premnath@gmail.com>
2023-04-14 16:46:39 +0530

    intial setup done

--- src/examples/BlackSquare.jsx
+++ src/examples/BlackSquare.jsx
@@ -2,23 +2,32 @@ import React, { useState, useEffect } from "react";
 import "./BlackSquare.css";
 
 export default function BlackSquare() {
-  const [arr, setArr] = useState([]);
-  useEffect(() => {
-    let array = [],
-      value = 1;
-    for (let i = 0; i < 60; i++) {
-      let obj = { id: value++, color: "black" };
-      array[i] = obj;
-    }
-    setArr(array);
-  }, []);
+  const [arr, setArr] = useState(getArray());
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    const changeColor = () => {};
+    const changeColor = () => {
+      setCount((c) => c + 1);
+    };
     const intervalId = setInterval(changeColor, 1000);
-
     return () => clearInterval(intervalId);
   }, []);
+
+  useEffect(() => {
+    console.log("count", count);
+    let array = [];
+    arr.forEach((item) => {
+      let obj = {};
+      obj["id"] = item.id;
+      if (item.id < count) {
+        obj["color"] = "white";
+      } else {
+        obj["color"] = "black";
+      }
+      array.push(obj);
+    });
+    setArr(array);
+  }, [count]);
   return (
     <div className="wrapper">
       {arr.map((item) => {
@@ -27,7 +36,15 @@ export default function BlackSquare() {
     </div>
   );
 }
-
+const getArray = () => {
+  let array = [],
+    value = 1;
+  for (let i = 0; i < 60; i++) {
+    let obj = { id: value++, color: "black" };
+    array[i] = obj;
+  }
+  return array;
+};
 const Box = ({ propColor }) => {
   return (
     <div
