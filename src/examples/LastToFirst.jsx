856400fcf5e9e63521b248f99f3d0e2357bdec22
sreen98 <sreenath.premnath@gmail.com>
2023-04-15 12:33:47 +0530

    Last to First Completed

--- src/examples/LastToFirst.jsx
+++ src/examples/LastToFirst.jsx
@@ -3,19 +3,22 @@ import React, { useState } from "react";
 function LastToFirst() {
   const [string, setString] = useState("");
 
-  const deBounceClick = () => {
-    setTimeout(() => {
-      setString([...string].reverse().join(""));
-    }, 1000);
-  };
   const onButtonClick = () => {
-    deBounceClick();
+    const array = string.split("");
+    const char = array.pop();
+    let newArray = [];
+    newArray.push(char);
+    newArray = [...newArray, ...array];
+    newArray = newArray.join("");
+    setString(newArray);
   };
   return (
-    <div>
+    <div style={{ padding: "100px 200px 300px 250px" }}>
       {string}
-      <input onChange={(e) => setString(e.target.value)} />
-      <button onClick={onButtonClick}>Click me </button>
+      <div>
+        <input onChange={(e) => setString(e.target.value)} />
+        <button onClick={onButtonClick}>Click me </button>
+      </div>
     </div>
   );
 }
